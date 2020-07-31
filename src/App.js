a6f45ff711ee7c61b9def8de0fc0297429d3d263
wildenali <wildenwildenaliali@gmail.com>
2020-07-31 23:08:13 +0700

    Menambahkan gambar pada Home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import mainImage from './assets/images/aneka-makanan.jpg'
 
 class App extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
-        <a class="navbar-brand" href="/">KulinerAn</a>
-      </nav>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top">
+          <a className="navbar-brand" href="/">KulinerAn</a>
+        </nav>
+        <div className="container-fluid">
+          <div className="row" style={{ marginBottom: 30 }}>
+            <img src={mainImage} width="100%" alt="aneka-makanan"></img>
+          </div>
+        </div>
+      </div>
     );
   }
 }
