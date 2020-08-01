bc69cd382654406f897e7c63af0616cad9d4b201
wildenali <wildenwildenaliali@gmail.com>
2020-08-01 09:48:20 +0700

    Membuat Component ImageAndWelcome

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import mainImage from './assets/images/aneka-makanan.jpg'
 import CityCard from './components/CityCard'
 import NavBar from './components/NavBar'
+import ImageAndWelcome from './components/ImageAndWelcome'
 
 class App extends Component {
   render() {
@@ -16,23 +16,7 @@ class App extends Component {
     return (
       <div>
         <NavBar />
-        <div className="container-fluid">
-          <div className="row" style={{ marginBottom: 30 }}>
-            <img src={mainImage} width="100%" alt="aneka-makanan"></img>
-          </div>
-          <div className="row">
-            <div className="col">
-              <div className="card text-white bg-primary mb-3 text-center">
-                <div className="card-header"><h1>Welcome to KulinerAn</h1></div>
-                <div className="card-body">
-                  <h4 className="card-title">The Easiest Way to Find Restaurants and Food</h4>
-                  <p className="card-text">You can grab information about restaurants, cafees, cuisines with just a few clicks.</p>
-                  <p className="card-text">Start by choosing the featured cities below, or search the city name.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ImageAndWelcome />
 
         <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
           <div className="row">
