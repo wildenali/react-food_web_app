0f64328d952b2bb04f163a7e93b548c360eef76c
wildenali <wildenwildenaliali@gmail.com>
2020-08-03 16:28:00 +0700

    Setting Router untuk halaman Restaurant Detail"

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import City from './pages/City';
+import RestaurantDetail from './pages/RestaurantDetail';
 
 class App extends Component {
 
@@ -14,6 +15,7 @@ class App extends Component {
         <NavBar />
         <Route path="/" exact component={Home}></Route>
         <Route path="/city/:city_id" component={City}></Route>
+        <Route path="/restaurant/:restaurant_id" component={RestaurantDetail}></Route>
         <Footer />
       </Router>
     );
