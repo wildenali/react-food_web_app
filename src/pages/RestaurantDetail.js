aa32822fa4c7fef199537f4b1c4543b3704bb673
wildenali <wildenwildenaliali@gmail.com>
2020-08-03 20:15:37 +0700

    Cara mendapatkan data restaurant

--- src/pages/RestaurantDetail.js
+++ src/pages/RestaurantDetail.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { API } from '../config/api'
 
 class RestaurantDetail extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      restaurant: null,
+    }
+  }
+
+  getRestaurantData = (restaurant_id) => {
+    let url = `${API.zomato.baseUrl}/restaurant`
+    axios.get(url, {
+      headers: {
+        'user-key': API.zomato.api_key
+      },
+      params: {
+        res_id: restaurant_id
+      }
+    })
+      .then(({ data }) => {
+        this.setState({ restaurant: data })
+      })
+      .catch(err => console.log(err))
+  }
+
+  componentDidMount() {
+    let { params } = this.props.match
+    this.getRestaurantData(params.restaurant_id)
+  }
+
   render() {
     return (
       <>
