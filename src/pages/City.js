ee70a80e609352d5bb518d13bdbf492a0b460854
wildenali <wildenwildenaliali@gmail.com>
2020-08-03 14:48:32 +0700

    Membuat component Restaurant Card

--- src/pages/City.js
+++ src/pages/City.js
@@ -4,7 +4,7 @@ import { API } from '../config/api'
 import CategoryList from '../components/CategoryList'
 import SearchKeyword from '../components/SearchKeyword'
 import SearchCriteria from '../components/SearchCriteria'
-import { Link } from 'react-router-dom'
+import RestaurantCard from '../components/RestaurantCard'
 
 
 // Categiries Dummmy Data
@@ -207,56 +207,7 @@ class City extends Component {
             <div className="row">
               {
                 restaurant.map(({ restaurant }) => (
-                  <div className="col-6" style={{ marginBottom: 20 }}>
-                    <div className="card">
-                      <div className="card-header">
-                        <div className="row">
-                          <div className="col-3">
-                            <img className="img-responsive" src={restaurant.thumb} alt="" style={{ borderRadius: 5, width: 100 }}></img>
-                          </div>
-                          <div className="col-9">
-                            <h4 className="text-success" style={{ fontWeight: 800 }}>{restaurant.name}</h4>
-                            <h6>{restaurant.location.locality}</h6>
-                            <h6 className="text-muted">{restaurant.location.address}</h6>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="card-body">
-                        <table className="table">
-                          <tbody>
-                            <tr>
-                              <td>Rating</td>
-                              <td>
-                                <div
-                                  className="btn btn-sm"
-                                  style={{
-                                    color: 'white',
-                                    backgroundColor: `#${restaurant.user_rating.rating_color}`,
-                                    borderColor: `#${restaurant.user_rating.rating_color}`,
-                                  }}
-                                >
-                                  {`${restaurant.user_rating.aggregate_rating} (${restaurant.user_rating.rating_text})`}
-                                </div>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Cuisines</td>
-                              <td>{restaurant.cuisines}</td>
-                            </tr>
-                            <tr>
-                              <td>Cost for two</td>
-                              <td>{restaurant.currency + ' ' + restaurant.average_cost_for_two}</td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                      <div className="card-footer">
-                        <Link to={`/restaurant/${restaurant.id}`} style={{ textDecoration: 'none' }}>
-                          <button type="button" className="btn btn-outline-success btn-block">View Restaurant Details</button>
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
+                  <RestaurantCard key={restaurant.id} restaurant={restaurant} />
                 ))
               }
             </div>
