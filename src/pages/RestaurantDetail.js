a535c9867016193d9f840d5f2ae9b25195359caf
wildenali <wildenwildenaliali@gmail.com>
2020-08-03 21:47:28 +0700

    Cara menampilkan reviews restaurant

--- src/pages/RestaurantDetail.js
+++ src/pages/RestaurantDetail.js
@@ -55,11 +55,48 @@ class RestaurantDetail extends Component {
   render() {
     return (
       <>
-        <p>Halaman Restaurant Detail</p>
+        <h4 className="text-center pt-5">Restaurant Detail</h4>
         <div className="container" style={{ marginTop: 30, marginBottom: 30 }}>
           <div className="row">
             <div className="col-12" style={{ marginBottom: 20 }}>
               <RestaurantProfile restaurant={this.state.restaurant} />
+              <div className="col-12" style={{ marginBottom: 20 }}>
+                <div className="card">
+                  <div className="card-body">
+                    <h4 className="text-success" style={{ fontWeight: 800 }}>Reviews</h4>
+                    {
+                      this.state.reviews ? (
+                        this.state.reviews.map(({ review }) => (
+                          <div className="card border-success" style={{ marginBottom: 5 }}>
+                            <div className="card-body">
+                              <div className="row" style={{ marginBottom: 20 }}>
+                                <div className="col-1" style={{ border: '0px solid black' }}>
+                                  <img class="img-responsive" src={review.user.profile_image} alt="" style={{ borderRadius: '50%', width: 80 }}></img>
+                                </div>
+                                <div className="col-11" style={{ border: '0px solid black' }}>
+                                  <h6 className="font-weight-bold">{review.user.name}</h6>
+                                  <RatingLabel
+                                    text={`${review.user.foodie_level_num} (${review.user.foodie_level})`}
+                                    labelColor={`${review.user.foodie_color}`}
+                                  />
+                                </div>
+                              </div>
+                              <h6 className="card-text text-muted">{review.review_time_friendly}</h6>
+                              <RatingLabel
+                                text={`${review.rating} (${review.rating_text})`}
+                                labelColor={`${review.rating_color}`}
+                              />
+                              <p className="card-text">{review.review_text}</p>
+                            </div>
+                          </div>
+                        ))
+                      ) : (
+                        <p>Loooooadiiing . . . .</p>
+                      )
+                    }
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
